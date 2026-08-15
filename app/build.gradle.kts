ee6b86c70f74c2c464fd9fd72a597ef1615124e9
beshtawiali <81652513+beshtawiali@users.noreply.github.com>
2026-08-15 05:11:49 +0300

    build: increment version code to 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
     applicationId = "com.tayf.photocompressor"
     minSdk = 24
     targetSdk = 36
-    versionCode = 2
+    versionCode = 3
     versionName = "1.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
